5ea2f027cda78230fbc12fea1e047b2d94ed1d7a
navras <moamen.zakaria@gmail.com>
2023-08-15 23:34:53 -0700

    skeleton

---
+++ src/main/java/com/kn/springwsdemo/SpringwsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.kn.springwsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwsdemoApplication.class, args);
+	}
+
+}
